4469e7e75b9d68b7fa3d5d85fdb48199037b2845
Yann Blanc <yann.blanc.38144@gmail.com>
2013-11-23 13:11:22 +0000

    Mise en place d'un composant personnalisé et utilisation dans index.xhtml

---
+++ Ecom-war/src/java/component/Vitrine.java
@@ -0,0 +1,40 @@
+package classes;
+
+import java.io.IOException;
+import javax.faces.context.FacesContext;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.ResponseWriter;
+
+@FacesComponent(value = "Vitrine")
+public class Vitrine extends UIComponentBase {
+
+    private String label;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+    
+    public String getComponentType(){
+        return("Vitrine");
+    }
+
+    @Override
+    public String getFamily() {
+        return "Vitrine";
+    }
+
+    @Override
+    public void encodeBegin(FacesContext context) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        writer.write("<h1>");
+        if (label != null) {
+            writer.write(label);
+        }
+        writer.write("</h1>");
+    }
+}
