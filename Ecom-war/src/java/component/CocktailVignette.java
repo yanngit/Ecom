4e1dab0cde8a3d8189b752d2d998206ee2ec8147
Yann Blanc <yann.blanc.38144@gmail.com>
2013-11-24 17:18:05 +0000

    Mise en place de bouttons pour ajouter les produits

--- Ecom-war/src/java/component/CocktailVignette.java
+++ Ecom-war/src/java/component/CocktailVignette.java
@@ -13,71 +13,70 @@ import javax.faces.context.ResponseWriter;
 
 @FacesComponent(value = "CocktailVignette")
 public class CocktailVignette extends UIComponentBase implements NamingContainer {
-
-    public String getID(){
+    
+    public String getID() {
         return (String) getStateHelper().eval("ID");
     }
     
-    public void setID(String ID){
+    public void setID(String ID) {
         getStateHelper().put("ID", ID);
     }
     
     public String getIdPrefix() {
         return (String) getStateHelper().eval("idPrefix");
     }
-
+    
     public void setIdPrefix(String idPrefix) {
         getStateHelper().put("idPrefix", idPrefix);
     }
-
+    
     public String getName() {
         return (String) getStateHelper().eval("name");
     }
-
+    
     public void setName(String name) {
         getStateHelper().put("name", name);
     }
-
+    
     public String getPrice() {
         return (String) getStateHelper().eval("price");
     }
-
+    
     public void setPrice(String price) {
         getStateHelper().put("price", price);
     }
-
+    
     public String getUrlPhoto() {
         return (String) getStateHelper().eval("urlPhoto");
     }
-
+    
     public void setUrlPhoto(String urlPhoto) {
         getStateHelper().put("urlPhoto", urlPhoto);
     }
-
+    
     @Override
     public String getFamily() {
         return "CocktailVignette";
     }
-            
+    
     @Override
     public void encodeBegin(FacesContext context) throws IOException {
         super.encodeBegin(context);
         ResponseWriter writer = context.getResponseWriter();
         String id = getIdPrefix();
         String[] tab = getName().split("\\s+");
-        for(String s : tab){
-            id+="_"+s;
+        for (String s : tab) {
+            id += "_" + s;
         }
-        writer.write("<div id=\"" + id+"\" class=\"span2 cocktail-vignette\">");
-        writer.write("<img src=\"resources/img/"+getUrlPhoto()+"\" alt=\"Cocktail "+getName()+"\" longdesc=\"chart.html\" /><br />");
-        writer.write("<h2 class=\"cocktail-vignette-name\">"+getName()+"</h2>");
-        writer.write("<h:commandButton label=\"Ajouter\" title=\"Ajouter au panier\" action=\"#{dataManagedBean.addArticleToCart(item.ID)}\"/>");
+        writer.write("<div id=\"" + id + "\" class=\"span2 cocktail-vignette\">");
+        writer.write("<img src=\"resources/img/" + getUrlPhoto() + "\" alt=\"Cocktail " + getName() + "\" longdesc=\"chart.html\" /><br />");
+        writer.write("<h2 class=\"cocktail-vignette-name\">" + getName() + "</h2>");
+    }
+    
+    @Override
+    public void encodeEnd(FacesContext context) throws IOException {
+        super.encodeBegin(context);
+        ResponseWriter writer = context.getResponseWriter();
         writer.write("</div>");
-        ELContext el = context.getELContext();
-        Application app = context.getApplication();
-        ExpressionFactory eFactory = app.getExpressionFactory();
-        CommandButtonTag c  = new CommandButtonTag();
-        c.setValue(eFactory.createValueExpression(el, "Ajouter",String.class));
-        c.setOnclick(eFactory.createValueExpression(el, "#{dataManagedBean.addArticleToCart(\"+getID()+\")}",String.class));
     }
 }
