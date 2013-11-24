d9967018786c08c06633a5f5fc94e25aa4032b2a
Yann Blanc <yann.blanc.38144@gmail.com>
2013-11-24 16:49:53 +0000

    Mise en place de composants composite pour agréger plusieurs composants JSF dans un seul, tested OK manque la mise en page des bouttons d'ajout au panier

--- Ecom-war/src/java/component/CocktailVignette.java
+++ Ecom-war/src/java/component/CocktailVignette.java
@@ -1,14 +1,27 @@
 package component;
 
+import com.sun.faces.taglib.html_basic.CommandButtonTag;
 import java.io.IOException;
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
+import javax.faces.application.Application;
 import javax.faces.context.FacesContext;
 import javax.faces.component.FacesComponent;
+import javax.faces.component.NamingContainer;
 import javax.faces.component.UIComponentBase;
 import javax.faces.context.ResponseWriter;
 
 @FacesComponent(value = "CocktailVignette")
-public class CocktailVignette extends UIComponentBase {
+public class CocktailVignette extends UIComponentBase implements NamingContainer {
 
+    public String getID(){
+        return (String) getStateHelper().eval("ID");
+    }
+    
+    public void setID(String ID){
+        getStateHelper().put("ID", ID);
+    }
+    
     public String getIdPrefix() {
         return (String) getStateHelper().eval("idPrefix");
     }
@@ -45,9 +58,10 @@ public class CocktailVignette extends UIComponentBase {
     public String getFamily() {
         return "CocktailVignette";
     }
-
+            
     @Override
     public void encodeBegin(FacesContext context) throws IOException {
+        super.encodeBegin(context);
         ResponseWriter writer = context.getResponseWriter();
         String id = getIdPrefix();
         String[] tab = getName().split("\\s+");
@@ -57,6 +71,13 @@ public class CocktailVignette extends UIComponentBase {
         writer.write("<div id=\"" + id+"\" class=\"span2 cocktail-vignette\">");
         writer.write("<img src=\"resources/img/"+getUrlPhoto()+"\" alt=\"Cocktail "+getName()+"\" longdesc=\"chart.html\" /><br />");
         writer.write("<h2 class=\"cocktail-vignette-name\">"+getName()+"</h2>");
+        writer.write("<h:commandButton label=\"Ajouter\" title=\"Ajouter au panier\" action=\"#{dataManagedBean.addArticleToCart(item.ID)}\"/>");
         writer.write("</div>");
+        ELContext el = context.getELContext();
+        Application app = context.getApplication();
+        ExpressionFactory eFactory = app.getExpressionFactory();
+        CommandButtonTag c  = new CommandButtonTag();
+        c.setValue(eFactory.createValueExpression(el, "Ajouter",String.class));
+        c.setOnclick(eFactory.createValueExpression(el, "#{dataManagedBean.addArticleToCart(\"+getID()+\")}",String.class));
     }
 }
