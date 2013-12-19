69f2af612112392c856387328745da8770bd2564
bachar nejem <bachar.nejem@gmail.com>
2013-12-19 17:27:59 +0100

    Verification de saisie FONCTIONEL

--- Ecom-war/web/resources/js/common.js
+++ Ecom-war/web/resources/js/common.js
@@ -16,21 +16,21 @@
 function validateConnexion() {
     var text = "";
     var valid = true;
-    if (!$("[id*=':login']").val().match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+    if (!$("[id*=':login']").val().match(/^[\w\-]+(\.[\w\-]+)*@[\w\-]+(\.[\w\-]+)*\.[\w\-]{2,4}$/)) {
         //$("[id*='login']").after('<p>Veuillez saisir une adresse email valide en tant qu\'identifiant</p>');
         text += "Veuillez saisir une adresse email valide en tant qu'identifiant \n";
         valid = false;
     }
-    if ($("[id*=':password']").val() == '' || $("[id*='form-connection:password']").val().length < 5) {
+    if ($("[id*=':password']").val() == '' || $("[id*=':password']").val().length < 5) {
         //$("[id*='login']").after('<p>Veuillez saisir une adresse email valide en tant qu\'identifiant</p>');
         text += "Votre mot de passe doit comporter au minimum 6 caractères \n";
         valid = false;
-    } else {
-        if (!$("[id*=':password']").val().match(/[A-Za-z]+[0-9]*/)) {
-            text += "Votre mot de passe doit comporter au minimum un chiffre une lettre et 4 caractères additionnels (chiffre ou lettre)\n";
-            valid = false;
-        }
+    } 
+    if (!$("[id*=':password']").val().match(/^(?=.*\d)(?=.*[a-zA-Z]).{6,12}$/)) {
+        text += "Votre mot de passe doit comporter au minimum un chiffre une lettre et 4-10 caractères additionnels (chiffre ou lettre)\n";
+        valid = false;
     }
+    
 
     if (valid) {
         return true;
